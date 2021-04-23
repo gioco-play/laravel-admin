f3c2a817181cb3e2dcdc16f305e0a5d95b0e7a7a
Xuan Yang <xuan.yang@goldenf.co>
2021-04-23 14:25:18 +0800

    fix tree parent_id bug

--- src/Traits/ModelTree.php
+++ src/Traits/ModelTree.php
@@ -145,7 +145,7 @@ trait ModelTree
      *
      * @return array
      */
-    protected function buildNestedArray(array $nodes = [], $parentId = 0)
+    protected function buildNestedArray(array $nodes = [], $parentId = '0')
     {
         $branch = [];
 
@@ -209,7 +209,7 @@ trait ModelTree
      * @param array $tree
      * @param int   $parentId
      */
-    public static function saveOrder($tree = [], $parentId = 0)
+    public static function saveOrder($tree = [], $parentId = '0')
     {
         if (empty(static::$branchOrder)) {
             static::setBranchOrder($tree);
